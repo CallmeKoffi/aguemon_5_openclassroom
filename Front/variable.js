08e804bdc7271c9eb06a086cbcc1ba6b524e29c0
Koffi <winston.aguemon@gmail.com>
2021-08-30 17:13:47 +0200

    Variable fetch js

--- Front/variable.js
+++ Front/variable.js
@@ -14,12 +14,12 @@ fetch(urlFirstCamera).then((res) =>
  })
  ).catch(err => alert('Erreur '+ err))
 
- // Fetch pour API deuxième article en page d'acceuil
+// Fetch pour API deuxième article en page d'acceuil
 
  const urlSecondCamera = 'http://localhost:3000/api/cameras/5be1ef211c9d44000030b062';
 
- fetch(urlSecondCamera).then((res) => 
-  res.json().then((data) => {
+fetch(urlSecondCamera).then((res) => 
+res.json().then((data) => {
      console.log(data.price);
      document.querySelector('#secondPicture').innerHTML = data.imageUrl;
      document.querySelector('#titleSecondProduct').innerHTML = data.name;
@@ -30,10 +30,10 @@ fetch(urlFirstCamera).then((res) =>
 
  // Fetch pour API troisième article en page d'acceuil
 
- const urlThirdCamera = 'http://localhost:3000/api/cameras/5be9bc241c9d440000a730e7 ';
+const urlThirdCamera = 'http://localhost:3000/api/cameras/5be9bc241c9d440000a730e7 ';
 
- fetch(urlThirdCamera).then((res) => 
-  res.json().then((data) => {
+fetch(urlThirdCamera).then((res) => 
+ res.json().then((data) => {
      console.log(data.price);
      document.querySelector('#thirdPicture').innerHTML = data.imageUrl;
      document.querySelector('#titleThirdProduct').innerHTML = data.name;
@@ -43,9 +43,9 @@ fetch(urlFirstCamera).then((res) =>
     ).catch(err => alert('Erreur '+ err))
 
  // Fetch pour API quatrième article en page d'acceuil
- const urlFourthCamera = 'http://localhost:3000/api/cameras/5be9c4471c9d440000a730e8';
+const urlFourthCamera = 'http://localhost:3000/api/cameras/5be9c4471c9d440000a730e8';
 
- fetch(urlFourthCamera).then((res) => 
+fetch(urlFourthCamera).then((res) => 
   res.json().then((data) => {
      console.log(data.price);
      document.querySelector('#fourthPicture').innerHTML = data.imageUrl;
@@ -57,9 +57,9 @@ fetch(urlFirstCamera).then((res) =>
 
 
  // Fetch pour API cinquième article en page d'acceuil
- const urlFifthCamera = 'http://localhost:3000/api/cameras/5be9c4c71c9d440000a730e9';
+const urlFifthCamera = 'http://localhost:3000/api/cameras/5be9c4c71c9d440000a730e9';
 
- fetch(urlFifthCamera).then((res) => 
+fetch(urlFifthCamera).then((res) => 
   res.json().then((data) => {
      console.log(data.price);
      document.querySelector('#fifthPicture').innerHTML = data.imageUrl;
