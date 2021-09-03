db12261e9de3fc203a2a7071da2fe85b4c718609
Koffi <winston.aguemon@gmail.com>
2021-09-03 19:08:19 +0200

    création page produit

--- Front/product.js
+++ Front/product.js
@@ -2,8 +2,8 @@ console.log((new URL(document.location)).searchParams.get("id"));
 const idProduct = (new URL(document.location)).searchParams.get("id")
 const urlcamera = ' http://localhost:3000/api/cameras/' + idProduct
 fetch(urlcamera).then((res) => 
-res.json().then((data) => {
-    console.log(data);
+res.json().then((product) => {
+    console.log(product);
      document.querySelector('#secondPicture').innerHTML = data.imageUrl;
      document.querySelector('#titleSecondProduct').innerHTML = data.name;
      document.querySelector('#priceSecondProduct').innerHTML = data.price + "€";
