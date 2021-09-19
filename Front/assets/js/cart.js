7faeb3116c867e18a8678a15016c6470a7862508
Koffi <winston.aguemon@gmail.com>
2021-09-19 13:41:44 +0200

    Affichage produit sélectionné dans panier

--- Front/assets/js/cart.js
+++ Front/assets/js/cart.js
@@ -1,8 +1,7 @@
 //----------------------- declaration variable afin d'avoir mon LS actif sur page cart---------
 
-let productsInCart = JSON.parse(localStorage.getItem("products"));
-
-
+let productsInCart = JSON.parse(localStorage.getItem('products'));
+let productDisplayInCart = [];
 //---------creation constante qui prend le container panier----------
 
 const cartContainer = document.querySelector('.container_cart');
@@ -10,22 +9,38 @@ const cartContainer = document.querySelector('.container_cart');
 //----------création message quand panier vide-------------
 
 if (productsInCart === null) {
-    const msgEmpty = `
+  const msgEmpty = `
      <div class="container_cart_empty">
         <div>
-            Le panier est <a href="/Front/index.html">Vide</a>
+            Le panier est Vide. <a href="/Front/index.html">Retournez à l'acceuil.</a>
         </div>
 
-    </div>` ;
-    cartContainer.innerHTML = msgEmpty;
-//---------------implantation produit choisis dans le containeur-------------
-
+    </div>`;
+  cartContainer.innerHTML = msgEmpty;
+  //---------------implantation produit choisis dans le containeur-------------
+  
 } else {
-   let productDisplayInCart = [];
-   for (let i = 0; i < productsInCart.length; i++) {
-       console.log(productsInCart)
-       
-   }
+  
+  for (let i = 0; i < productsInCart.length; i++) {
+    productDisplayInCart = productDisplayInCart + `
+        
+        <div class= "container_class-info" >
+           <div >
+              Nom article : ${productsInCart[i].name}
+            
+           </div>
+           
+           <div>
+              Prix : ${productsInCart[i].price} €
+           </div>
 
+           <div>
+              <button> <i class="fas fa-trash-alt"></i> </button>
+           </div>
 
+        </div>
+        `;
+      
+  }
+  cartContainer.innerHTML = productDisplayInCart;
 }
